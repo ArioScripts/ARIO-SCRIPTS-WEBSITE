597a1fbb09ab2bcb4f2b219d3b388a816d719947
Ario <arioscripts@gmail.com>
2026-09-10 21:17:32 +0200

    Update next.config.ts

--- ario-scripts/next.config.ts
+++ ario-scripts/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: { remotePatterns: [{ protocol: "https", hostname: "**" }] }
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
